0f8072493aec19782537d923fd0201f3abad7b42
Aleksey Bykhun <theisease@gmail.com>
2018-07-22 11:22:17 +0300

    * all origins

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ const safe = (handler) => async (req, res) => {
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
