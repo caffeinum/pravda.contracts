a828ded28a315e1a75c7f5b9d2eff225eb5695bf
Aleksey Bykhun <theisease@gmail.com>
2018-07-22 11:20:52 +0300

    + access allow origin

--- server/app.js
+++ server/app.js
@@ -23,6 +23,26 @@ const safe = (handler) => async (req, res) => {
   }
 }
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+})
+
 app.get('/echo', safe((wallet) => wallet))
 
 app.get('/balance', safe(async ({ address }) => {
